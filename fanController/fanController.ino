a3608be8e1b1463831db115d0c1e2d63d0410f14
Eric <46936873+Eric-Bwr@users.noreply.github.com>
2024-03-19 06:14:44 +0100

    Update fanController.ino

--- fanController/fanController.ino
+++ fanController/fanController.ino
@@ -1,9 +1,9 @@
 const int FAN_LEFT_PIN = 3;
 const int FAN_RIGHT_PIN = 6;
-const int FAN_LEFT_MIN = 60;
+const int FAN_LEFT_MIN = 10;
 const int FAN_LEFT_MAX = 255;
 const int FAN_RIGHT_MIN = 10;
-const int FAN_RIGHT_MAX = 220;
+const int FAN_RIGHT_MAX = 255;
 
 void setup() {
   Serial.begin(9600);
@@ -15,21 +15,16 @@ void loop() {
    if (Serial.available() > 0) {
     int speed = Serial.parseInt();
     Serial.read();
-    setFanSpeed(speed);
+    setFanSpeed(speed, FAN_LEFT_MIN, FAN_LEFT_MAX, FAN_LEFT_PIN);
+    setFanSpeed(speed, FAN_RIGHT_MIN, FAN_RIGHT_MAX, FAN_RIGHT_PIN);
   }
   delay(100);
 }
 
-void setFanSpeed(int percent){
+void setFanSpeed(int percent, int min, int max, int pin){
   int percentClamped = clamp(percent, 0, 100);
-  int pwm = map(percentClamped, 0, 100, FAN_LEFT_MIN, FAN_LEFT_MAX);
-  
-  analogWrite(FAN_LEFT_PIN, pwm);
-
-  percentClamped = clamp(percent, 0, 100);
-  pwm = map(percentClamped, 0, 100, FAN_RIGHT_MIN, FAN_RIGHT_MAX);
-
-  analogWrite(FAN_RIGHT_PIN, pwm);
+  int pwm = map(percentClamped, 0, 100, min, max);
+  analogWrite(pin, pwm);
 }
 
 int clamp(int value, int minVal, int maxVal) {
