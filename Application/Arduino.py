b3a88f1d15f8c1a72e4afe8e44be97ee3b1a6574
Eric-Bwr <boewer2003@gmail.com>
2024-10-06 17:27:33 +0200

    Added engine rpm vibration motor

--- Application/Arduino.py
+++ Application/Arduino.py
@@ -2,16 +2,19 @@ import time
 import serial.tools.list_ports
 from Util import *
 
-ARDUINO_IN_MAX = 300
-
+SPEED_IN_MAX_KMH = 300
+RPM_IN_MAX_PERC = 100
 
 def runArduino(sharedData):
     arduino = serial.Serial(getArduinoPort(), 9600, write_timeout=0.1)
     print("Connected to Arduino.", flush=True)
     while True:
-        clampedSpeed = clampValue(sharedData[KEY_SPEED], 0.0, ARDUINO_IN_MAX)
-        mappedSpeed = mapValue(clampedSpeed, 0.0, ARDUINO_IN_MAX, 0.0, 100)
-        arduino.write(f"{int(mappedSpeed)}\n".encode())
+        sharedData[KEY_ENGINE_RPM_PERC]
+        clampedSpeed = clampValue(sharedData[KEY_SPEED], 0.0, SPEED_IN_MAX_KMH)
+        mappedSpeed = mapValue(clampedSpeed, 0.0, SPEED_IN_MAX_KMH, 0.0, 100)
+        clampedRPM = clampValue(sharedData[KEY_ENGINE_RPM_PERC] * 100.0, 0.0, RPM_IN_MAX_PERC)
+        mappedRPM = mapValue(clampedRPM, 0.0, RPM_IN_MAX_PERC, 0.0, 100)
+        arduino.write(f"{int(mappedSpeed)},{int(mappedRPM)}\n".encode())
         time.sleep(0.1)
 
 
@@ -31,3 +34,4 @@ def getArduinoPort():
         print("No Sim-Racing setup found, waiting...")
 
         time.sleep(1.0)
+
